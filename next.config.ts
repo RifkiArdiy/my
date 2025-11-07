fa2259406bbb3aca60c827303ea55f54c8fe2812
RifkiArdiy <ardiyrifki@gmail.com>
2025-11-07 21:49:28 +0700

    refactor: update next.config.ts for production settings and GitHub Pages compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
-import type { NextConfig } from "next";
+// next.config.js
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "my"; // <= GANTI dgn nama repo
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  basePath: '/my',
-
+module.exports = {
+  output: "export",            // supaya bisa dihost di GitHub Pages (static)
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   images: {
+    // next/image saat export butuh ini, atau pakai <img>
     unoptimized: true,
+    // Kalau ambil dari domain eksternal, whitelist juga:
+    // domains: ["images.unsplash.com", "res.cloudinary.com"],
   },
+  // opsional, kadang membantu routing di Pages
+  trailingSlash: true,
 };
-
-export default nextConfig;
