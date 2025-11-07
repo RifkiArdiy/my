703f1905a118aee438ce5b5cf892866f7146c34a
RifkiArdiy <ardiyrifki@gmail.com>
2025-11-07 22:44:22 +0700

    refactor: add environment variable for base path during Next.js build process

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
-// next.config.js
 const isProd = process.env.NODE_ENV === "production";
 const repoName = "my";
+
+// Pastikan variable tersedia saat proses build Next.js
+process.env.NEXT_PUBLIC_BASE_PATH = isProd ? `/${repoName}` : "";
 
 module.exports = {
   output: "export",
