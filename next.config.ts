654edd13e5db092181f771759e4b72b3f60149c1
RifkiArdiy <ardiyrifki@gmail.com>
2025-11-07 22:21:22 +0700

    refactor: update next.config.ts and package.json for improved configuration and added shx dependency

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
 // next.config.js
 const isProd = process.env.NODE_ENV === "production";
-const repoName = "my"; // <= GANTI dgn nama repo
+const repoName = "my"; // ganti sesuai nama repo kamu
 
+/** @type {import('next').NextConfig} */
 module.exports = {
-  output: "export",            // supaya bisa dihost di GitHub Pages (static)
+  output: "export",
   basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
-  images: {
-    // next/image saat export butuh ini, atau pakai <img>
-    unoptimized: true,
-    // Kalau ambil dari domain eksternal, whitelist juga:
-    // domains: ["images.unsplash.com", "res.cloudinary.com"],
-  },
-  // opsional, kadang membantu routing di Pages
+  images: { unoptimized: true },
   trailingSlash: true,
 };
